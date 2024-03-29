eb357c0b2b1b46e74ab1bd51264db4e9a135cc53
CharlottaG <DELETED-1691214276921>
2024-03-29 14:38:57 +0000

    Add functionality to edit and delete a comment you made

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('patterns/', views.PatternList.as_view(), name='pattern_list'),
     path('<slug:slug>/', views.pattern_details, name="pattern_details"),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
     path('profile/', views.Profile.as_view(), name='profile'),
 ]
