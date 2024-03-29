d5b03ec81bbdaabfad278534e1435dc05d838e94
CharlottaG <DELETED-1691214276921>
2024-03-29 11:08:26 +0000

    Add Pattern model in blog and add cloudinary

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
+
+# Create your models here.
+
+class Pattern(models.Model):
+    pattern_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.CharField(max_length=200, default="description")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="patterncreator")
+    featured_image = CloudinaryField('image', default='placeholder')
+    needle_size = models.CharField(max_length=50)
+    gauge = models.CharField(max_length=50)
+    yarn = models.CharField(max_length=50, default="yarn")
+    difficulity_level = models.IntegerField(choices=LEVEL,)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+
+    def __str__(self):
+        return f"{self.pattern_name} | by {self.created_by}"
--- knittersnook/settings.py
+++ knittersnook/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lxchu0)6!c1&fhf$kt(bnr)#k*7vsd_r0%zy$$=a9_7vum+#j7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-charlottag-knittersnook-x9l8ygrkqk.us1.codeanyapp.com', '.heroku.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
